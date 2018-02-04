c5816417fdb551f2e51cb4196fdf9d230f6d8ba1
Victor Usoltsev <bernarden@mail.ru>
2018-02-04 17:06:48 +1300

    Added ILogger and LoggingSeverityLevel enum.

---
+++ Source/Vima.LoggingAbstractor.Core/ILogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vima.LoggingAbstractor.Core
+{
+    /// <summary>
+    /// Represents an instance of a logger. 
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Traces the message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void TraceMessage(string message);
+
+        /// <summary>
+        /// Traces the message.
+        /// </summary>
+        /// <param name="message">The message to be logged.</param>
+        /// <param name="loggingSeverityLevel">The logging severity level.</param>
+        void TraceMessage(string message, LoggingSeverityLevel loggingSeverityLevel);
+
+        /// <summary>
+        /// Traces the exception.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        void TraceException(Exception exception);
+
+        /// <summary>
+        /// Traces the exception.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        /// <param name="loggingSeverityLevel">The logging severity level.</param>
+        void TraceException(Exception exception, LoggingSeverityLevel loggingSeverityLevel);
+    }
+}
