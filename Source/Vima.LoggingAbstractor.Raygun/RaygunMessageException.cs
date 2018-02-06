6c8d08c4bef717b3b4f0f05fdcc974bc6555d667
Bernarden <bernarden@users.noreply.github.com>
2018-02-06 23:28:46 +1300

    Added code analysis.

--- Source/Vima.LoggingAbstractor.Raygun/RaygunMessageException.cs
+++ Source/Vima.LoggingAbstractor.Raygun/RaygunMessageException.cs
@@ -19,7 +19,8 @@ namespace Vima.LoggingAbstractor.Raygun
         /// Initializes a new instance of the <see cref="RaygunMessageException"/> class.
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
-        public RaygunMessageException(string message) : base(message)
+        public RaygunMessageException(string message)
+            : base(message)
         {
         }
 
@@ -28,7 +29,8 @@ namespace Vima.LoggingAbstractor.Raygun
         /// </summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
-        public RaygunMessageException(string message, Exception innerException) : base(message, innerException)
+        public RaygunMessageException(string message, Exception innerException)
+            : base(message, innerException)
         {
         }
     }
