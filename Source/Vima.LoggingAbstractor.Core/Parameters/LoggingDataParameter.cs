b4c543217b99b46af7dfd54c3a9e2620a86bbee5
Bernarden <bernarden@users.noreply.github.com>
2018-03-14 22:31:06 +1300

    Adds data parameter. Adds Multilogger. Updates packages.

---
+++ Source/Vima.LoggingAbstractor.Core/Parameters/LoggingDataParameter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Vima.LoggingAbstractor.Core.Parameters
+{
+    /// <summary>
+    /// Represents logging data parameter.
+    /// </summary>
+    public class LoggingDataParameter : ILoggingParameter<object>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggingDataParameter"/> class.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public LoggingDataParameter(object data)
+        {
+            Value = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+        /// <summary>
+        /// Gets the parameter's value.
+        /// </summary>
+        /// <value>
+        /// The parameter's value.
+        /// </value>
+        public object Value { get; }
+
+        /// <summary>
+        /// Gets the type of the logging parameter.
+        /// </summary>
+        /// <value>
+        /// The type of the logging parameter.
+        /// </value>
+        public LoggingParameterType LoggingParameterType => LoggingParameterType.Data;
+    }
+}
---
+++ Source/Vima.LoggingAbstractor.Raygun/IRaygunLogger.cs
@@ -0,0 +1,11 @@
+﻿using Vima.LoggingAbstractor.Core;
+
+namespace Vima.LoggingAbstractor.Raygun
+{
+    /// <summary>
+    /// Represents an instance of a Raygun logger.
+    /// </summary>
+    public interface IRaygunLogger : ILogger
+    {
+    }
+}
