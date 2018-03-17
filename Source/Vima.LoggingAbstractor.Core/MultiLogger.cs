100d85235db88d0fa6f10bcef93db98f89055b5b
Victor Usoltsev <bernarden@mail.ru>
2018-03-17 13:19:14 +1300

    Changes MultiLogger constructor to be public.

--- Source/Vima.LoggingAbstractor.Core/MultiLogger.cs
+++ Source/Vima.LoggingAbstractor.Core/MultiLogger.cs
@@ -16,7 +16,7 @@ namespace Vima.LoggingAbstractor.Core
         /// Initializes a new instance of the <see cref="MultiLogger"/> class.
         /// </summary>
         /// <param name="loggers">The loggers used to trace events.</param>
-        protected MultiLogger(IEnumerable<ILogger> loggers)
+        public MultiLogger(IEnumerable<ILogger> loggers)
         {
             _loggers = loggers ?? throw new ArgumentNullException(nameof(loggers));
         }
