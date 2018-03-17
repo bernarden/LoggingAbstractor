6de38fc89a7d8abddfe1df7882c5f68ddafffeb4
Victor Usoltsev <bernarden@mail.ru>
2018-03-17 15:10:57 +1300

    Adds console and no-op loggers.

--- Source/Vima.LoggingAbstractor.Core/MultiLogger/IMultiLogger.cs
+++ Source/Vima.LoggingAbstractor.Core/MultiLogger/IMultiLogger.cs
@@ -1,8 +1,9 @@
-﻿namespace Vima.LoggingAbstractor.Core
+﻿namespace Vima.LoggingAbstractor.Core.MultiLogger
 {
     /// <summary>
     /// Responsible for combining multiple loggers at the same time.
     /// </summary>
+    /// <seealso cref="Vima.LoggingAbstractor.Core.ILogger" />
     public interface IMultiLogger : ILogger
     {
     }
--- Source/Vima.LoggingAbstractor.Core/NoOpLogger/NoOpLogger.cs
+++ Source/Vima.LoggingAbstractor.Core/NoOpLogger/NoOpLogger.cs
@@ -1,33 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Vima.LoggingAbstractor.Core.Parameters;
 
-namespace Vima.LoggingAbstractor.Core
+namespace Vima.LoggingAbstractor.Core.NoOpLogger
 {
     /// <summary>
-    /// Responsible for combining multiple loggers at the same time.
+    /// Represents an instance of a logger that performs no operations.
     /// </summary>
-    public class MultiLogger : IMultiLogger
+    /// <seealso cref="Vima.LoggingAbstractor.Core.NoOpLogger.INoOpLogger" />
+    public class NoOpLogger : INoOpLogger
     {
-        private readonly IEnumerable<ILogger> _loggers;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MultiLogger"/> class.
-        /// </summary>
-        /// <param name="loggers">The loggers used to trace events.</param>
-        protected MultiLogger(IEnumerable<ILogger> loggers)
-        {
-            _loggers = loggers ?? throw new ArgumentNullException(nameof(loggers));
-        }
-
         /// <summary>
         /// Traces the message.
         /// </summary>
         /// <param name="message">The message to be logged.</param>
         public void TraceMessage(string message)
         {
-            TraceMessage(message, LoggingLevel.Verbose);
         }
 
         /// <summary>
@@ -37,7 +25,6 @@ namespace Vima.LoggingAbstractor.Core
         /// <param name="loggingLevel">The logging level.</param>
         public void TraceMessage(string message, LoggingLevel loggingLevel)
         {
-            TraceMessage(message, loggingLevel, Enumerable.Empty<ILoggingParameter>());
         }
 
         /// <summary>
@@ -48,11 +35,6 @@ namespace Vima.LoggingAbstractor.Core
         /// <param name="parameters">The logging parameters.</param>
         public void TraceMessage(string message, LoggingLevel loggingLevel, IEnumerable<ILoggingParameter> parameters)
         {
-            IEnumerable<ILoggingParameter> loggingParameters = parameters.ToList();
-            foreach (var logger in _loggers)
-            {
-                logger.TraceMessage(message, loggingLevel, loggingParameters);
-            }
         }
 
         /// <summary>
@@ -61,7 +43,6 @@ namespace Vima.LoggingAbstractor.Core
         /// <param name="exception">The exception to be logged.</param>
         public void TraceException(Exception exception)
         {
-            TraceException(exception, LoggingLevel.Critical);
         }
 
         /// <summary>
@@ -71,7 +52,6 @@ namespace Vima.LoggingAbstractor.Core
         /// <param name="loggingLevel">The logging level.</param>
         public void TraceException(Exception exception, LoggingLevel loggingLevel)
         {
-            TraceException(exception, loggingLevel, Enumerable.Empty<ILoggingParameter>());
         }
 
         /// <summary>
@@ -82,11 +62,6 @@ namespace Vima.LoggingAbstractor.Core
         /// <param name="parameters">The logging parameters.</param>
         public void TraceException(Exception exception, LoggingLevel loggingLevel, IEnumerable<ILoggingParameter> parameters)
         {
-            IEnumerable<ILoggingParameter> loggingParameters = parameters.ToList();
-            foreach (var logger in _loggers)
-            {
-                logger.TraceException(exception, loggingLevel, loggingParameters);
-            }
         }
     }
 }
