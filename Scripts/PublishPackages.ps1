0b3bf9eb671a464018702acbdedb3ec9343371ae
Bernarden <bernarden@users.noreply.github.com>
2018-03-22 00:42:31 +1300

    Increases version number to 0.2.0. Adds package publishing script.

---
+++ Scripts/PublishPackages.ps1
@@ -0,0 +1,56 @@
+$ErrorActionPreference = "Stop"
+$PackagesDir = Join-Path -Path $PSScriptRoot -ChildPath "../Artifacts/Packages"
+$NugetDir = Join-Path -Path $PSScriptRoot -ChildPath "../Temp/nuget.exe"
+
+function Main {
+    $packages = Get-ChildItem -Path $PackagesDir
+    Write-Host "Following packages were found:" -ForegroundColor Yellow
+    Write-Host $packages -Separator "`n" 
+
+    $result = Get-Approval-To-Execute
+    switch ($result) {
+        0 { 
+            Write-Host "Continuing..." -ForegroundColor Yellow
+            Start-Sleep -s 10
+            Publish-Packages $packages 
+        }
+            
+        1 {
+            Write-Host "Exiting..." -ForegroundColor Yellow
+        }
+    }
+}
+
+function Get-Approval-To-Execute {
+    $title = "Publish Packages"
+    $message = "Are you sure you want to publish the packages listed above to nuget.org?"
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Packages will be published!"
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Script execution will stop."
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+    return $host.ui.PromptForChoice($title, $message, $options, 1) 
+}
+
+function Publish-Packages {
+    param ( [System.Array] $Packages)
+
+    $nugetExeFile = Get-Item -Path $NugetDir
+
+    $symbolsPackagesPattern = ".*.symbols.nupkg"
+    $nugetPackagesPattern = ".*(?<!\.symbols)\.nupkg"
+    
+    $symbolsPackages = $Packages | Where-Object {$_.Name -match $symbolsPackagesPattern}
+    foreach ($symbolsPackage in $symbolsPackages) {
+        Write-Host "Publishing $($symbolsPackage.Name)" -ForegroundColor Yellow  
+        $args = @("push", $symbolsPackage.FullName, "-source", "https://nuget.smbsrc.net/")
+        & $nugetExeFile.FullName $args
+    }
+
+    $nugetPackages = $Packages | Where-Object {$_.Name -match $nugetPackagesPattern}
+    foreach ($nugetPackage in $nugetPackages) {
+        Write-Host "Publishing $($nugetPackage.Name)" -ForegroundColor Yellow
+        $args = @("push", $nugetPackage.FullName, "-source", "https://api.nuget.org/v3/index.json")
+        & $nugetExeFile.FullName $args
+    }
+}
+
+Main
