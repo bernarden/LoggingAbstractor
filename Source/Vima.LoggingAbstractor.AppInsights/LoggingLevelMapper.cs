08a8f3b9ab719c8de347982a2fff1c5c099db309
Bernarden <bernarden@users.noreply.github.com>
2018-03-25 20:53:53 +1300

    Adds Logging level mappings for all clients.

---
+++ Source/Vima.LoggingAbstractor.AppInsights/LoggingLevelMapper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+using Vima.LoggingAbstractor.Core;
+
+namespace Vima.LoggingAbstractor.AppInsights
+{
+    /// <summary>
+    /// Responsible for mapping <see cref="LoggingLevel"/> to <see cref="SeverityLevel"/>.
+    /// </summary>
+    internal class LoggingLevelMapper
+    {
+        /// <summary>
+        /// Converts the <see cref="LoggingLevel"/> to <see cref="SeverityLevel"/>.
+        /// </summary>
+        /// <param name="loggingLevel">The logging level.</param>
+        /// <returns>Application Insights severity level.</returns>
+        internal static SeverityLevel ConvertLoggingLevelToSeverityLevel(LoggingLevel loggingLevel)
+        {
+            switch (loggingLevel)
+            {
+                case LoggingLevel.Verbose:
+                    return SeverityLevel.Verbose;
+                case LoggingLevel.Information:
+                    return SeverityLevel.Information;
+                case LoggingLevel.Warning:
+                    return SeverityLevel.Warning;
+                case LoggingLevel.Error:
+                    return SeverityLevel.Error;
+                case LoggingLevel.Critical:
+                    return SeverityLevel.Critical;
+                default:
+                    return SeverityLevel.Verbose;
+            }
+        }
+    }
+}
---
+++ Source/Vima.LoggingAbstractor.Sentry/LoggingLevelMapper.cs
@@ -0,0 +1,35 @@
+﻿using SharpRaven.Data;
+using Vima.LoggingAbstractor.Core;
+
+namespace Vima.LoggingAbstractor.Sentry
+{
+    /// <summary>
+    /// Responsible for mapping <see cref="LoggingLevel"/> to <see cref="ErrorLevel"/>.
+    /// </summary>
+    internal static class LoggingLevelMapper
+    {
+        /// <summary>
+        /// Converts the <see cref="LoggingLevel"/> to <see cref="ErrorLevel"/>.
+        /// </summary>
+        /// <param name="loggingLevel">The logging level.</param>
+        /// <returns>Sentry error level.</returns>
+        internal static ErrorLevel ConvertLoggingLevelToErrorLevel(LoggingLevel loggingLevel)
+        {
+            switch (loggingLevel)
+            {
+                case LoggingLevel.Verbose:
+                    return ErrorLevel.Debug;
+                case LoggingLevel.Information:
+                    return ErrorLevel.Info;
+                case LoggingLevel.Warning:
+                    return ErrorLevel.Warning;
+                case LoggingLevel.Error:
+                    return ErrorLevel.Error;
+                case LoggingLevel.Critical:
+                    return ErrorLevel.Fatal;
+                default:
+                    return ErrorLevel.Debug;
+            }
+        }
+    }
+}
