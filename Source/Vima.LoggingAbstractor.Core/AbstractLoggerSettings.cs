d6509a90aa490004927701cb192e06f0c2949104
Bernarden <bernarden@users.noreply.github.com>
2018-06-11 03:52:37 +1200

    Adds contrsuctors with settings parameter.

---
+++ Source/Vima.LoggingAbstractor.Core/AbstractLoggerSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Vima.LoggingAbstractor.Core
+{
+    /// <summary>
+    /// Represents an instance of settings.
+    /// </summary>
+    public class AbstractLoggerSettings
+    {
+        /// <summary>
+        /// Gets or sets the minimal logging lever required for a log to go through.
+        /// </summary>
+        /// <value>
+        /// The minimal logging lever required for a log to go through.
+        /// </value>
+        public LoggingLevel MinimalLoggingLevel { get; set; }
+    }
+}
--- Source/Vima.LoggingAbstractor.Raygun/RaygunAbstractLogger.cs
+++ Source/Vima.LoggingAbstractor.Raygun/RaygunAbstractLogger.cs
@@ -20,10 +20,21 @@ namespace Vima.LoggingAbstractor.Raygun
         /// <summary>
         /// Initializes a new instance of the <see cref="RaygunAbstractLogger"/> class.
         /// </summary>
-        /// <param name="raygunClient">The raygun client.</param>
+        /// <param name="raygunClient">The Raygun client.</param>
         /// <param name="minimalLoggingLevel">The minimal logging level.</param>
         public RaygunAbstractLogger(RaygunClient raygunClient, LoggingLevel minimalLoggingLevel = LoggingLevel.Verbose)
-            : base(minimalLoggingLevel)
+            : base(new AbstractLoggerSettings { MinimalLoggingLevel = minimalLoggingLevel })
+        {
+            _raygunClient = raygunClient ?? throw new ArgumentNullException(nameof(raygunClient));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RaygunAbstractLogger"/> class.
+        /// </summary>
+        /// <param name="raygunClient">The Raygun client.</param>
+        /// <param name="settings">The logger's settings.</param>
+        public RaygunAbstractLogger(RaygunClient raygunClient, AbstractLoggerSettings settings)
+            : base(settings ?? throw new ArgumentNullException(nameof(settings)))
         {
             _raygunClient = raygunClient ?? throw new ArgumentNullException(nameof(raygunClient));
         }
