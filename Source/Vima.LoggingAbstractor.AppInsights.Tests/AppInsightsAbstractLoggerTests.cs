297af7781ef1ec725c3b65d9f351222642d27bb2
Victor Usoltsev <bernarden@mail.ru>
2018-12-04 20:06:27 +1300

    Updates AppInsights logger. Adds a test.

---
+++ Source/Vima.LoggingAbstractor.AppInsights.Tests/AppInsightsAbstractLoggerTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Vima.LoggingAbstractor.Core;
+using Vima.LoggingAbstractor.Core.Parameters;
+using Xunit;
+
+namespace Vima.LoggingAbstractor.AppInsights.Tests
+{
+    public sealed class AppInsightsAbstractLoggerTests
+    {
+        private const string ApiKey = "";
+
+        private static AppInsightsAbstractLogger CreateAppInsightsAbstractLogger(out TelemetryClient telemetryClient)
+        {
+            if (string.IsNullOrEmpty(ApiKey))
+            {
+                throw new ArgumentNullException(nameof(ApiKey));
+            }
+
+            telemetryClient = new TelemetryClient(new TelemetryConfiguration(ApiKey));
+            var appInsightsLogger = new AppInsightsAbstractLogger(telemetryClient);
+            return appInsightsLogger;
+        }
+
+        public class TraceException
+        {
+            [Fact(Skip = "Needs an Application Insights ApiKey.")]
+            public void ShouldTraceExceptionWithTags()
+            {
+                // Arrange
+                var appInsightsLogger = CreateAppInsightsAbstractLogger(out var telemetryClient);
+                var exception = new Exception("Test-" + DateTime.UtcNow.ToString("s"));
+
+                LoggingTagsParameter loggingTagsParameter = new LoggingTagsParameter(new[] { "tag", "tag2" });
+                var loggingParameters = new List<ILoggingParameter> { loggingTagsParameter };
+
+                // Act
+                appInsightsLogger.TraceException(exception, LoggingLevel.Critical, loggingParameters);
+                telemetryClient.Flush();
+
+                // Assert
+                // Manually validate that it was created correctly.
+            }
+        }
+    }
+}
