74eb3eb5346f7233bb3a57b6d6afc601da818793
Victor Usoltsev <bernarden@mail.ru>
2018-12-05 22:10:08 +1300

    Adds LoggerIdentityParameter.

--- Source/Vima.LoggingAbstractor.Core.Tests/LoggingParameterExtensionsTest.cs
+++ Source/Vima.LoggingAbstractor.Core.Tests/LoggingParameterExtensionsTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using Newtonsoft.Json;
 using Vima.LoggingAbstractor.Core.Extensions;
 using Vima.LoggingAbstractor.Core.Parameters;
 using Xunit;
@@ -15,11 +16,8 @@ namespace Vima.LoggingAbstractor.Core.Tests
             [Fact]
             public void ShouldHandleNoParameters()
             {
-                // Arrange
-                var loggingParameters = new List<ILoggingParameter> { new LoggingDataParameter(string.Empty) };
-
                 // Act
-                var tags = loggingParameters.ExtractTags().ToList();
+                var tags = new List<ILoggingParameter>().ExtractTags().ToList();
 
                 // Assert
                 tags.Should().BeEmpty();
@@ -28,8 +26,11 @@ namespace Vima.LoggingAbstractor.Core.Tests
             [Fact]
             public void ShouldHandleNoTagsParameters()
             {
+                // Arrange
+                var loggingParameters = new List<ILoggingParameter> { new LoggingDataParameter("Data") };
+
                 // Act
-                var tags = new List<ILoggingParameter>().ExtractTags().ToList();
+                var tags = loggingParameters.ExtractTags().ToList();
 
                 // Assert
                 tags.Should().BeEmpty();
@@ -56,14 +57,153 @@ namespace Vima.LoggingAbstractor.Core.Tests
                 // Arrange
                 var loggingTagsParameter1 = new LoggingTagsParameter(new List<Enum> { LoggingLevel.Critical });
                 var loggingTagsParameter2 = new LoggingTagsParameter(new List<Enum> { LoggingLevel.None });
+                var loggingParameters = new List<ILoggingParameter> { loggingTagsParameter1, loggingTagsParameter2 };
 
                 // Act
-                var tags = new List<ILoggingParameter> { loggingTagsParameter1, loggingTagsParameter2 }.ExtractTags().ToList();
+                var tags = loggingParameters.ExtractTags().ToList();
 
                 // Assert
                 tags.Count.Should().Be(2);
                 tags.Should().Contain(LoggingLevel.Critical.ToString("G"));
                 tags.Should().Contain(LoggingLevel.None.ToString("G"));
+            }
+        }
+
+        public sealed class ExtractData
+        {
+            [Fact]
+            public void ShouldHandleNoParameters()
+            {
+                // Act
+                var data = new List<ILoggingParameter>().ExtractData().ToList();
+
+                // Assert
+                data.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void ShouldHandleNoDataParameters()
+            {
+                // Arrange
+                var loggingParameters = new[] { new LoggingTagsParameter(new List<string> { "CustomTag" }) };
+
+                // Act
+                var data = loggingParameters.ExtractData().ToList();
+
+                // Assert
+                data.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void ShouldHandleOneDataParameter()
+            {
+                // Arrange
+                var dataValue = "Data";
+                var loggingParameters = new List<ILoggingParameter> { new LoggingDataParameter(dataValue) };
+
+                // Act
+                var data = loggingParameters.ExtractData().ToList();
+
+                // Assert
+                data.Count.Should().Be(1);
+                data.Should().Contain(JsonConvert.SerializeObject(dataValue));
+            }
+
+            [Fact]
+            public void ShouldHandleMultipleDataParameter()
+            {
+                // Arrange
+                var dataValue1 = "Data 1";
+                var dataValue2 = "Data 2";
+                var loggingParameters = new List<ILoggingParameter> { new LoggingDataParameter(dataValue1), new LoggingDataParameter(dataValue2) };
+
+                // Act
+                var data = loggingParameters.ExtractData().ToList();
+
+                // Assert
+                data.Count.Should().Be(2);
+                data.Should().Contain(JsonConvert.SerializeObject(dataValue1));
+                data.Should().Contain(JsonConvert.SerializeObject(dataValue2));
+            }
+        }
+
+        public sealed class ExtractIdentity
+        {
+            [Fact]
+            public void ShouldHandleNoParameters()
+            {
+                // Act
+                var identity = new List<ILoggingParameter>().ExtractIdentity();
+
+                // Assert
+                identity.Should().BeNull();
+            }
+
+            [Fact]
+            public void ShouldHandleNoIdentityParameters()
+            {
+                // Arrange
+                var loggingParameters = new[] { new LoggingTagsParameter(new List<string> { "CustomTag" }) };
+
+                // Act
+                var identity = loggingParameters.ExtractIdentity();
+
+                // Assert
+                identity.Should().BeNull();
+            }
+
+            [Fact]
+            public void ShouldHandleIdentityParameter()
+            {
+                // Arrange
+                var identityValue = "Id";
+                var identityName = "Name";
+                var loggingParameters = new List<ILoggingParameter> { new LoggingIdentityParameter(identityValue, identityName) };
+
+                // Act
+                var identity = loggingParameters.ExtractIdentity();
+
+                // Assert
+                identity.Should().NotBeNull();
+                identity.Identity.Should().Be(identityValue);
+                identity.Name.Should().Be(identityName);
+            }
+
+            [Fact]
+            public void ShouldHandleIdentityParameterWithoutName()
+            {
+                // Arrange
+                var identityValue = "Id";
+                var loggingParameters = new List<ILoggingParameter> { new LoggingIdentityParameter(identityValue) };
+
+                // Act
+                var identity = loggingParameters.ExtractIdentity();
+
+                // Assert
+                identity.Should().NotBeNull();
+                identity.Identity.Should().Be(identityValue);
+                identity.Name.Should().BeNull();
+            }
+
+            [Fact]
+            public void ShouldHandleMultipleIdentityParameter()
+            {
+                // Arrange
+                var identityValue = "Id";
+                var identityName = "Name";
+                var loggingParameters = new List<ILoggingParameter>
+                {
+                    new LoggingIdentityParameter(identityValue, identityName),
+                    new LoggingIdentityParameter($"{identityValue}-2", $"{identityName}-2")
+                };
+
+                // Act
+                var identity = loggingParameters.ExtractIdentity();
+
+                // Assert
+                identity.Should().NotBeNull();
+                identity.Identity.Should().Be(identityValue);
+                identity.Name.Should().Be(identityName);
             }
         }
     }
---
+++ Source/Vima.LoggingAbstractor.Core/Parameters/LoggingIdentityParameter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Vima.LoggingAbstractor.Core.Parameters
+{
+    /// <summary>
+    /// Represents logging identity parameter.
+    /// </summary>
+    public class LoggingIdentityParameter : ILoggingParameter<IdentityParameter>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggingIdentityParameter"/> class.
+        /// </summary>
+        /// <param name="identity">The identity.</param>
+        /// <param name="name">The name.</param>
+        public LoggingIdentityParameter(string identity, string name = null)
+        {
+            if (identity == null)
+            {
+                throw new ArgumentNullException(nameof(identity));
+            }
+
+            Value = new IdentityParameter(identity, name);
+        }
+
+        /// <summary>
+        /// Gets the parameter's value.
+        /// </summary>
+        /// <value>
+        /// The parameter's value.
+        /// </value>
+        public IdentityParameter Value { get; }
+
+        /// <summary>
+        /// Gets the type of the logging parameter.
+        /// </summary>
+        /// <value>
+        /// The type of the logging parameter.
+        /// </value>
+        public LoggingParameterType LoggingParameterType => LoggingParameterType.Identity;
+    }
+}
