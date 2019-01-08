bc53a02f4bc08a11c52eaab8f7bce3852f0ff3fa
Victor Usoltsev <bernarden@mail.ru>
2019-01-09 01:40:15 +1300

    Adds references to Sentry test project.

--- Source/Vima.LoggingAbstractor.Sentry.Tests/UnitTest1.cs
+++ Source/Vima.LoggingAbstractor.Sentry.Tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System;
+using Sentry;
+using Vima.LoggingAbstractor.Core;
 using Xunit;
 
 namespace Vima.LoggingAbstractor.Sentry.Tests
@@ -7,6 +10,22 @@ namespace Vima.LoggingAbstractor.Sentry.Tests
         [Fact]
         public void Test1()
         {
+            // var client = new SentryClient(new SentryOptions { Dsn = new Dsn(""), BeforeSend = BeforeSend });
+
+            // client.CaptureEvent(new SentryEvent() { Message = "Hi" });
+            // var logger = new SentryAbstractLogger(client);
+
+            // logger.TraceException(new AccessViolationException());
+            // logger.TraceMessage("Test4");
+
+            // logger.TraceMessage("Hello2");
+            // logger.TraceMessage("Test3");
+            // client.Dispose();
         }
+
+        // private SentryEvent BeforeSend(SentryEvent arg)
+        // {
+        //    return arg;
+        // }
     }
 }
