e4bd4ab5a04fa83df7ce21cd20ab6417643baba9
Victor Usoltsev <bernarden@mail.ru>
2019-01-09 00:01:24 +1300

    Adds SentryAbstractLogger that uses the new Sentry SDK.

--- Source/Vima.LoggingAbstractor.Sentry/LoggingLevelMapper.cs
+++ Source/Vima.LoggingAbstractor.Sentry/LoggingLevelMapper.cs
@@ -1,10 +1,14 @@
-﻿using SharpRaven.Data;
+﻿#if SentrySDK
+using Sentry.Protocol;
+#endif
+
+using SharpRaven.Data;
 using Vima.LoggingAbstractor.Core;
 
 namespace Vima.LoggingAbstractor.Sentry
 {
     /// <summary>
-    /// Responsible for mapping <see cref="LoggingLevel"/> to <see cref="ErrorLevel"/>.
+    /// Responsible for mapping <see cref="LoggingLevel"/> to respective Sentry level.
     /// </summary>
     internal static class LoggingLevelMapper
     {
@@ -31,5 +35,31 @@ namespace Vima.LoggingAbstractor.Sentry
                     return ErrorLevel.Debug;
             }
         }
+
+#if SentrySDK
+        /// <summary>
+        /// Converts the <see cref="LoggingLevel"/> to <see cref="SentryLevel"/>.
+        /// </summary>
+        /// <param name="loggingLevel">The logging level.</param>
+        /// <returns>Sentry error level.</returns>
+        internal static SentryLevel ConvertLoggingLevelToSentryLevel(LoggingLevel loggingLevel)
+        {
+            switch (loggingLevel)
+            {
+                case LoggingLevel.Verbose:
+                    return SentryLevel.Debug;
+                case LoggingLevel.Information:
+                    return SentryLevel.Info;
+                case LoggingLevel.Warning:
+                    return SentryLevel.Warning;
+                case LoggingLevel.Error:
+                    return SentryLevel.Error;
+                case LoggingLevel.Critical:
+                    return SentryLevel.Fatal;
+                default:
+                    return SentryLevel.Debug;
+            }
+        }
+#endif
     }
 }
---
+++ Source/Vima.LoggingAbstractor.Sentry/SentryAbstractLogger.cs
@@ -0,0 +1,105 @@
+﻿#if SentrySDK
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sentry;
+using Vima.LoggingAbstractor.Core;
+using Vima.LoggingAbstractor.Core.Extensions;
+using Vima.LoggingAbstractor.Core.Parameters;
+
+namespace Vima.LoggingAbstractor.Sentry
+{
+    /// <summary>
+    /// Represents an instance of a Sentry logger.
+    /// </summary>
+    /// <seealso cref="AbstractLoggerBase" />
+    /// <seealso cref="ISentryAbstractLogger" />
+    public class SentryAbstractLogger : AbstractLoggerBase, ISentryAbstractLogger
+    {
+        private readonly IHub _hub;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SentryAbstractLogger"/> class.
+        /// </summary>
+        /// <param name="hub">The Sentry hub.</param>
+        /// <param name="minimalLoggingLevel">The minimal logging level.</param>
+        public SentryAbstractLogger(IHub hub, LoggingLevel minimalLoggingLevel = LoggingLevel.Verbose)
+            : base(new AbstractLoggerSettings { MinimalLoggingLevel = minimalLoggingLevel })
+        {
+            _hub = hub ?? throw new ArgumentNullException(nameof(hub));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SentryAbstractLogger"/> class.
+        /// </summary>
+        /// <param name="hub">The Sentry hub.</param>
+        /// <param name="settings">The logger's settings.</param>
+        public SentryAbstractLogger(IHub hub, AbstractLoggerSettings settings)
+            : base(settings ?? throw new ArgumentNullException(nameof(settings)))
+        {
+            _hub = hub ?? throw new ArgumentNullException(nameof(hub));
+        }
+
+        /// <summary>
+        /// Traces the message.
+        /// </summary>
+        /// <param name="message">The message to be logged.</param>
+        /// <param name="loggingLevel">The logging level.</param>
+        /// <param name="parameters">The logging parameters.</param>
+        public override void TraceMessage(string message, LoggingLevel loggingLevel, IEnumerable<ILoggingParameter> parameters)
+        {
+            if (!ShouldBeTraced(loggingLevel))
+            {
+                return;
+            }
+
+            _hub.WithScope(scope =>
+            {
+                SetupSentryScope(loggingLevel, parameters, scope);
+
+                _hub.CaptureMessage(message);
+            });
+        }
+
+        /// <summary>
+        /// Traces the exception.
+        /// </summary>
+        /// <param name="exception">The exception to be logged.</param>
+        /// <param name="loggingLevel">The logging level.</param>
+        /// <param name="parameters">The logging parameters.</param>
+        public override void TraceException(Exception exception, LoggingLevel loggingLevel, IEnumerable<ILoggingParameter> parameters)
+        {
+            if (!ShouldBeTraced(loggingLevel))
+            {
+                return;
+            }
+
+            _hub.WithScope(scope =>
+            {
+                SetupSentryScope(loggingLevel, parameters, scope);
+
+                _hub.CaptureException(exception);
+            });
+        }
+
+        private static Dictionary<string, string> GenerateTags(IEnumerable<ILoggingParameter> parameters)
+        {
+            return parameters.ExtractTags().ToDictionary(tag => tag);
+        }
+
+        private void SetupSentryScope(LoggingLevel loggingLevel, IEnumerable<ILoggingParameter> parameters, Scope scope)
+        {
+            var loggingParameters = GetGlobalAndLocalLoggingParameters(parameters);
+            Dictionary<string, string> tags = GenerateTags(loggingParameters);
+            scope.SetTags(tags);
+            scope.Level = LoggingLevelMapper.ConvertLoggingLevelToSentryLevel(loggingLevel);
+
+            var extraCount = 1;
+            foreach (string data in loggingParameters.ExtractData())
+            {
+                scope.SetExtra($"Extra #{extraCount++}", data);
+            }
+        }
+    }
+}
+#endif
